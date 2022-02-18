28d72c9f8d3edd6a1a5bd8d34fa0c14b1d921f28
Charlotte Du <33757670+charlottedyyt@users.noreply.github.com>
2022-02-19 01:08:41 +0800

    Update backup.py
    partially worked!!!

--- backup.py
+++ backup.py
@@ -23,38 +23,10 @@ def positionShift(originalList,x,y):
 
 for face_landmarks in face_landmarks_list:
     d = ImageDraw.Draw(im, 'RGBA')
-    #del my_list[1:5]
-
-    #position movement
-  #  left_eyebrowArray = np.array(face_landmarks['left_eyebrow'])
-  #  right_eyebrowArray = np.array(face_landmarks['right_eyebrow'])
-  #  left_eyeArray = np.array(face_landmarks['left_eye'])
-  #  right_eyeArray = np.array(face_landmarks['right_eye'])
 
     #move the evebrow +(6,2)
-    shiftEyebrow = np.array([6,2])
-    shiftedLeft_eyebrow = np.add(left_eyebrowArray,shiftEyebrow)
-    shiftedRight_eyebrow = np.add(right_eyebrowArray,shiftEyebrow)
-    shiftedLeft_eye = np.add(left_eyeArray,shiftEyebrow)
-    shiftedRight_eye = np.add(right_eyeArray,shiftEyebrow)
-    #
-    shiftedLeftIris = shiftedLeft_eye
-    shiftedRightIris = shiftedRight_eye
-    del leftIris[0]
-    del leftIris[2]
-    del rightIris[0]
-    del rightIris[2]
-    #convert to tuple
-    shiftedLeft_eye = tuple(map(tuple, shiftedLeft_eye))
-    ((2, 2), (2, -2))
-    shiftedRight_eye = tuple(map(tuple, shiftedRight_eye))
-    ((2, 2), (2, -2))
-    shiftedLeft_eyebrow = tuple(map(tuple, shiftedLeft_eyebrow))
-    ((2, 2), (2, -2))
-    shiftedRight_eyebrow = tuple(map(tuple, shiftedRight_eyebrow))
-    ((2, 2), (2, -2))
-
-
+    shiftedLeft_eyebrow = positionShift(face_landmarks['left_eyebrow'],6,2)
+    shiftedRight_eyebrow = positionShift(face_landmarks['right_eyebrow'],6,2)
     #draw the yellow shadow of eyebrows
     d.line(shiftedLeft_eyebrow, fill=(255, 247, 64, 1000), width=5)
     d.line(shiftedRight_eyebrow, fill=(255, 247, 64, 1000), width=5)
@@ -62,39 +34,39 @@ for face_landmarks in face_landmarks_list:
     d.line(face_landmarks['left_eyebrow'], fill=(255, 67, 67, 1000), width=5)
     d.line(face_landmarks['right_eyebrow'], fill=(255, 67, 67, 1000), width=5)
 
+    #move the eyes
+    left_eyelist = face_landmarks['left_eye']
+    right_eyelist = face_landmarks['right_eye']
+    shiftedLeft_eyes = positionShift(left_eyelist,6,4)
+    shiftedRight_eyes = positionShift(right_eyelist,6,4)
     # Draw the eye shadows
+    shiftedLeft_iris = shiftedLeft_eyes[1] + shiftedLeft_eyes[2] + shiftedLeft_eyes[4] + shiftedLeft_eyes[5]
+    shiftedRight_iris = shiftedRight_eyes[1] + shiftedRight_eyes[2] + shiftedRight_eyes[4] + shiftedRight_eyes[5]
+    d.polygon(shiftedLeft_iris, fill=(255, 90, 67, 1000))
+    d.polygon(shiftedRight_iris, fill=(255, 90, 67, 1000))
+    d.line(shiftedLeft_eyes + (shiftedLeft_eyes[0]), fill=(255, 90, 67, 1000), width=5)
+    d.line(shiftedRight_eyes + (shiftedRight_eyes[0]), fill=(255, 90, 67, 1000), width=5)
+    # Draw the eyes
+    left_iris = left_eyelist[1] + left_eyelist[2] + left_eyelist[4] + left_eyelist[5]
+    right_iris = right_eyelist[1] + right_eyelist[2] + right_eyelist[4] + right_eyelist[5]
+    d.polygon(left_iris, fill=(0, 46, 210, 1000))
+    d.polygon(right_iris, fill=(0, 46, 210, 1000))
+    d.line(left_eyelist + [left_eyelist[0]], fill=(0, 46, 210, 1000), width=5)
+    d.line(right_eyelist + [right_eyelist[0]], fill=(0, 46, 210, 1000), width=5)
+  
+    #Draw the nose shadow
+    shadow_List_nose = [face_landmarks['nose_bridge'][0]] + [face_landmarks['nose_tip'][0]] + [face_landmarks['nose_tip'][4]] 
+    tip_list_nose = [face_landmarks['nose_bridge'][0]] + [face_landmarks['nose_tip'][0]] + [face_landmarks['nose_tip'][4]] + [face_landmarks['nose_bridge'][0]] + [face_landmarks['nose_bridge'][3]] + [face_landmarks['nose_tip'][4]]
+    shadow_List_nose = positionShift(shadow_List_nose,8,5)
+    d.polygon(shadow_List_nose, fill=(255, 247, 64, 1000))
+    d.line(tip_list_nose, fill=(0, 46, 210, 1000), width=5)
 
-    #draw iris
-    d.polygon(leftIris, fill=(255, 90, 67, 1000))
-    d.polygon(leftIris, fill=(255, 90, 67, 1000))
-    
-    d.line(shiftedLeft_eye + [shiftedLeft_eye[0]], fill=(255, 90, 67, 1000), width=5)
-    d.line(shiftedRight_eye + [shiftedRight_eye[0]], fill=(255, 90, 67, 1000), width=5)
-    
-    #Draw the eyes
-    leftIris = face_landmarks['left_eye']
-    rightIris = face_landmarks['right_eye']
-    del leftIris[0]
-    del leftIris[2]
-    del rightIris[0]
-    del rightIris[2]
-    d.polygon(leftIris, fill=(0, 46, 210, 1000))
-    d.polygon(leftIris, fill=(0, 46, 210, 1000))
-    
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 46, 210, 1000), width=5)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 46, 210, 1000), width=5)
- 
-
-
-    # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=1)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=1)
 
     # Gloss the lips
     #d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
     #d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=2)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=2)
+    d.line(face_landmarks['top_lip'], fill=(0, 46, 210, 1000), width=5)
+    d.line(face_landmarks['bottom_lip'], fill=(0, 46, 210, 1000), width=5)
     
     #  for facial_feature in face_landmarks.keys():
     #     print(facial_feature)
@@ -102,16 +74,9 @@ for face_landmarks in face_landmarks_list:
     #         print ("序号：%s   值：%s" % (face_landmarks[facial_feature].index(i), i))
     
 
-    # Draw the noses
-   # d.polygon(face_landmarks['nose_bridge'], fill=(150, 0, 0, 128))
-    #d.polygon(face_landmarks['nose_tip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['nose_bridge'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['nose_tip'], fill=(150, 0, 0, 64), width=8)
-
-
     # draw the chin
-    d.polygon(face_landmarks['chin'], fill=(255, 255, 255, 30))
-    d.line(face_landmarks['chin'], fill=(150, 0, 0, 64), width=8)
+    list_chin = [face_landmarks['chin'][0]] + [face_landmarks['chin'][5]] + [face_landmarks['chin'][7]] + [face_landmarks['chin'][9]] + [face_landmarks['chin'][11]] + [face_landmarks['chin'][16]]
+    d.line(list_chin, fill=(0, 46, 210, 1000), width=5)
 
 #    pil_image.show()
     display(im)
